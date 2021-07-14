02c6e2d460ed197186cd8d49d612835af7e1427b
linuxrun1 <81863011+linuxrun1@users.noreply.github.com>
2021-07-14 14:03:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let path = require('path')
 let fs = require('fs')
 const CFonts = require('cfonts')
 
-CFonts.say('Biasalah...', {
+CFonts.say('Bot WA', {
     font: 'chrome',
     align: 'center',
     gradient: ['red', 'magenta']
